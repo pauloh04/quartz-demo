bb199470d39a9e949d6502132410a792dd1d389d
pauloh04 <pauloh04@gmail.com>
2018-06-01 12:24:16 -0300

    fix properties

--- src/main/java/com/opencodez/QuartzDemoApplication.java
+++ src/main/java/com/opencodez/QuartzDemoApplication.java
@@ -2,10 +2,8 @@ package com.opencodez;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({ "classpath:dao-context.xml" })
 public class QuartzDemoApplication {
 
 	public static void main(String[] args) {
