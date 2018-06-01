a5dfc8dfcfc5392b8fc717d3b9c441f630f46169
pauloh04 <pauloh04@gmail.com>
2018-05-31 21:40:34 -0300

    first commit

---
+++ src/main/java/com/opencodez/util/AppConstants.java
@@ -0,0 +1,19 @@
+package com.opencodez.util;
+
+public class AppConstants {
+	
+	public static final String BLANK = "";
+	
+	public static final String COLON = " : ";
+
+	public static final String DASH = " - ";
+	
+	public static final String KEY_TXNID = "TXNID";
+	
+	public static final String XPATH_TXNID = "//transcationId";
+	
+	public static final String ENTRY = "Entry";
+	
+	public static final String EXIT = "Exit";
+
+}
